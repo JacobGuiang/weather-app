166ed78d7801d5027329729f8ca59ba4fb27f80e
JacobGuiang <jacob.guiang1@gmail.com>
2022-08-29 17:59:19 -0500

    Create location form and add functions to populate country selection

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ async function loadJson(url) {
 }
 
 async function getWeather(location) {
-  let weatherObj;
+  let weatherObj = {};
   try {
     const weatherData = await loadJson(
       `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=58f2669ab93d58441800526b124f17d2`
@@ -24,6 +24,45 @@ async function getWeather(location) {
   return weatherObj;
 }
 
+async function getCountries() {
+  const countriesObj = {};
+  try {
+    const countriesData = await loadJson("https://restcountries.com/v3.1/all");
+    countriesData.forEach((country) => {
+      const name = country.name.common;
+      const code = country.ccn3;
+      countriesObj[name] = code;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  return countriesObj;
+}
+
+async function loadCountries() {
+  const countrySelect = document.getElementById("countrySelect");
+  const countriesObj = await getCountries();
+  Object.entries(countriesObj)
+    .sort()
+    .forEach((country) => {
+      const option = document.createElement("option");
+      const name = country[0];
+      const code = country[1];
+      option.innerText = name;
+      option.value = code;
+      countrySelect.appendChild(option);
+    });
+}
+
+// TO DO
+// function readSelect() {
+//   const countrySelect = document.getElementById("countrySelect");
+//   countrySelect.addEventListener("change", (option) => {
+//     const countrySelection = option.target.value;
+
+//   });
+// }
+
 (async () => {
-  console.log(await getWeather("dallas"));
+  await loadCountries();
 })();
