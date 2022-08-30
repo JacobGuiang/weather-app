a45daba6a21370f34a979c138f414dbddfcd17a1
JacobGuiang <jacob.guiang1@gmail.com>
2022-08-30 17:43:15 -0500

    Add state selection functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function loadJson(url) {
-  const response = await fetch(url, { mode: "cors" });
+  const response = await fetch(url, { mode: 'cors' });
   if (response.status === 200) {
     return response.json();
   }
@@ -27,7 +27,7 @@ async function getWeather(location) {
 async function getCountries() {
   const countriesObj = {};
   try {
-    const countriesData = await loadJson("https://restcountries.com/v3.1/all");
+    const countriesData = await loadJson('https://restcountries.com/v3.1/all');
     countriesData.forEach((country) => {
       const name = country.name.common;
       const code = country.ccn3;
@@ -40,12 +40,12 @@ async function getCountries() {
 }
 
 async function loadCountries() {
-  const countrySelect = document.getElementById("countrySelect");
+  const countrySelect = document.getElementById('country-select');
   const countriesObj = await getCountries();
   Object.entries(countriesObj)
     .sort()
     .forEach((country) => {
-      const option = document.createElement("option");
+      const option = document.createElement('option');
       const name = country[0];
       const code = country[1];
       option.innerText = name;
@@ -54,15 +54,92 @@ async function loadCountries() {
     });
 }
 
-// TO DO
-// function readSelect() {
-//   const countrySelect = document.getElementById("countrySelect");
-//   countrySelect.addEventListener("change", (option) => {
-//     const countrySelection = option.target.value;
+function renderStateSelection() {
+  const countrySelect = document.getElementById('country-select');
+  const formState = document.getElementById('form-state');
+  countrySelect.addEventListener('change', (option) => {
+    const countrySelection = option.target.value;
+    if (countrySelection === '840') {
+      formState.classList.remove('hidden');
+    } else {
+      formState.classList.add('hidden');
+    }
+  });
+}
 
-//   });
-// }
+function loadStates() {
+  const statesArray = [
+    'AL',
+    'AK',
+    'AS',
+    'AZ',
+    'AR',
+    'CA',
+    'CO',
+    'CT',
+    'DE',
+    'DC',
+    'FM',
+    'FL',
+    'GA',
+    'GU',
+    'HI',
+    'ID',
+    'IL',
+    'IN',
+    'IA',
+    'KS',
+    'KY',
+    'LA',
+    'ME',
+    'MH',
+    'MD',
+    'MA',
+    'MI',
+    'MN',
+    'MS',
+    'MO',
+    'MT',
+    'NE',
+    'NV',
+    'NH',
+    'NJ',
+    'NM',
+    'NY',
+    'NC',
+    'ND',
+    'MP',
+    'OH',
+    'OK',
+    'OR',
+    'PW',
+    'PA',
+    'PR',
+    'RI',
+    'SC',
+    'SD',
+    'TN',
+    'TX',
+    'UT',
+    'VT',
+    'VI',
+    'VA',
+    'WA',
+    'WV',
+    'WI',
+    'WY',
+  ];
+  const stateSelect = document.getElementById('state-select');
+  statesArray.forEach((state) => {
+    const option = document.createElement('option');
+    option.value = state;
+    option.innerText = state;
+    stateSelect.appendChild(option);
+  });
+}
 
 (async () => {
-  await loadCountries();
+  loadCountries();
+  renderStateSelection();
+  loadStates();
 })();
